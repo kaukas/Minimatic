6eef0f9b59e4e4d8aed66ad69b7959f23821a2a5
Linas Juškevičius <linas@idiles.com>
2009-10-14 15:54:50 +0300

    Version change

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ import re
 
 setup(
     name='MinificationWebHelpers',
-    version='0.3.5',
+    version='0.0.1',
     description='CSS and Javascript Minification/Combination Upgrade to WebHelpers',
     long_description=open('README.txt').read(),
     author='Pedro Algarvio',
     author_email='ufs@ufsoft.org',
     maintainer='Domen Kozar',
     maintainer_email='domen@dev.si',
-    url='http://docs.fubar.si/minwebhelpers/',
+    url='http://bitbucket.org/kaukas/minwebhelpers/',
     install_requires=["Pylons", "WebHelpers", "beaker", "cssutils"],
     tests_require=['nose'],
     test_suite='nose.collector',
