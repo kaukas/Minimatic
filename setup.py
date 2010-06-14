e5a587ed0f0bccd3e2a19ffdc0bb8413880cc4e7
Linas Juškevičius <linas@idiles.com>
2010-06-14 13:45:19 +0300

    Switch from HG to GIT and update config files

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
 from setuptools import setup, find_packages
-import re
+from os.path import join, dirname
+import sys
+# Fool distutils to accept more than ASCII
+reload(sys).setdefaultencoding('utf-8')
 
 setup(
     name='Minimatic',
     version='1.0',
     description='CSS and Javascript Minification/Combination Upgrade to WebHelpers',
-    long_description=open('README.txt').read(),
+    long_description=open(join(dirname(__file__), 'README.rst')).read(),
     author='Pedro Algarvio',
     author_email='ufs@ufsoft.org',
-    maintainer='Linas Juskevicius',
+    maintainer='Linas Juškevičius',
     maintainer_email='linas@idiles.com',
     install_requires=["Pylons", "WebHelpers", "beaker", "cssutils"],
     tests_require=['nose'],
